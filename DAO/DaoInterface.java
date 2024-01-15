410b726ad82e15eb46a22df4e2b3b5fe41467794
DangHoangPhi <hoangphi1232003@gmail.com>
2024-01-15 19:21:31 +0700

    create DaoInterface

---
+++ DAO/DaoInterface.java
@@ -0,0 +1,12 @@
+package DAO;
+
+import java.util.ArrayList;
+
+public interface DaoInterface <T>{
+    public int insert(T t);
+    public int update(T t);
+    public int delete(String t);
+    public ArrayList<T> selectAll();
+    public T selectById(String t);
+    int getAutoIncrement();
+}
