20e5f4316285033294d186e7bfb24b1acf506165
DangHoangPhi <hoangphi1232003@gmail.com>
2024-02-25 19:56:20 +0700

    update ChiTietHoaDon_Interface

--- Interface/ChiTietHoaDon_Interface.java
+++ Interface/ChiTietHoaDon_Interface.java
@@ -4,4 +4,5 @@ import DTO.ChiTietHoaDon;
 
 public interface ChiTietHoaDon_Interface {
     public boolean themChiTietHoaDon(ChiTietHoaDon cthd);
+    public boolean xoaChiTietHoaDon(String maHD);
 }
