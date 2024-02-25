365fe17a4d371b1e6282f798e76b32990c458c0c
DangHoangPhi <hoangphi1232003@gmail.com>
2024-02-25 19:58:18 +0700

    add interface to resolve luutamhoadon,themhoadonluutam,getHoaDon

--- Interface/HoaDon_Interface.java
+++ Interface/HoaDon_Interface.java
@@ -9,5 +9,7 @@ public interface HoaDon_Interface {
     public boolean themHoaDon(HoaDon hoaDon, ArrayList<ChiTietHoaDon> danhSachCTHD);
     public HoaDon selectByMaHD(String maHD);
     public String getAutoIncrement();
-
+    public boolean luuTamHoaDon(HoaDon hoaDon, ArrayList<ChiTietHoaDon> danhSachCTHD);
+    public boolean themHoaDonLuuTam(HoaDon hoaDon, ArrayList<ChiTietHoaDon> danhSachCTHD);
+    public HoaDon getHoaDonTheoMaHD(String maHD);
 }
