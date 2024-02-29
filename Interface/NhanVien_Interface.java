a9cbde8745202c283345fa306f6e2ba720c64d93
DangHoangPhi <hoangphi1232003@gmail.com>
2024-02-29 16:03:37 +0700

    add interface findBySDT

--- Interface/NhanVien_Interface.java
+++ Interface/NhanVien_Interface.java
@@ -9,6 +9,7 @@ public interface NhanVien_Interface{
     public Boolean checkNV(String email, String sdt) throws SQLException;
     public NhanVienDTO getNV(String sdt) throws SQLException ;
     NhanVienDTO selectById(String id);
+    NhanVienDTO findBySDT(String sdt);
     boolean update(NhanVienDTO updateNV);
     boolean insert(NhanVienDTO insertNV);
     ArrayList<NhanVienDTO> selectAll();
