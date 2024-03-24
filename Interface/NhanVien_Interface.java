f950e049ac0a97e8a886aedcb40564f5173e2645
DangHoangPhi <hoangphi1232003@gmail.com>
2024-03-24 20:08:10 +0700

    update NhanVien interface

--- Interface/NhanVien_Interface.java
+++ Interface/NhanVien_Interface.java
@@ -1,17 +1,17 @@
 package Interface;
 
-import DTO.NhanVienDTO;
+import DTO.NhanVien;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface NhanVien_Interface{
     public Boolean checkNV(String email, String sdt) throws SQLException;
-    public NhanVienDTO getNV(String sdt) throws SQLException ;
-    NhanVienDTO selectById(String id);
-    NhanVienDTO findBySDT(String sdt);
-    boolean update(NhanVienDTO updateNV);
-    boolean insert(NhanVienDTO insertNV);
-    ArrayList<NhanVienDTO> selectAll();
+    public NhanVien getNV(String sdt) throws SQLException ;
+    NhanVien selectById(String id);
+    NhanVien findBySDT(String sdt);
+    boolean update(NhanVien updateNV);
+    boolean insert(NhanVien insertNV);
+    ArrayList<NhanVien> selectAll();
     String getAutoIncrement();
 }
