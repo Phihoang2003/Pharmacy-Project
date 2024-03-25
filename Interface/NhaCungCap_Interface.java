cac2b393c9d86090f5eebb9dedffa6989272d660
DangHoangPhi <hoangphi1232003@gmail.com>
2024-03-25 21:50:46 +0700

    create NhaCungCap interface

---
+++ Interface/NhaCungCap_Interface.java
@@ -0,0 +1,18 @@
+package Interface;
+
+import DTO.NhaCungCap;
+
+import java.util.ArrayList;
+
+public interface NhaCungCap_Interface {
+    public ArrayList<NhaCungCap> selectAll();
+    public boolean insert(NhaCungCap ncc);
+    public ArrayList<NhaCungCap> timKiemNCC(String ma);
+    public boolean update(NhaCungCap maNCC);
+    public ArrayList<NhaCungCap> layDSNCCDangNhap();
+    public String layTenNhaCungCapTheoMa(String maNhaCungCap);
+    public String layMaNhaCungCapTheoTen(String tenNhaCungCap);
+
+    public NhaCungCap selectById(String id);
+    public String getAutoIncrement();
+}
