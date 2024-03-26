249eaa48db8eb1fc634cb3f110988b2019903254
DangHoangPhi <hoangphi1232003@gmail.com>
2024-03-26 21:32:09 +0700

    create PhieuNhap interface

---
+++ Interface/PhieuNhap_Interface.java
@@ -0,0 +1,19 @@
+package Interface;
+
+import DTO.PhieuNhap;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public interface PhieuNhap_Interface {
+    public ArrayList<PhieuNhap> selectAll();
+
+    public boolean insert(PhieuNhap mhn);
+
+    public ArrayList<PhieuNhap> timKiemMHN(Date ngayNhap);
+
+    public boolean update(PhieuNhap mhn);
+
+    public boolean kiemTraMaMatHangNhapTonTai(String maMHN);
+    public String getAutoIncrement();
+}
