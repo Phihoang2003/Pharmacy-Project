4696e5cb6ab41e56d5d0680d402ca286b5e4ce55
DangHoangPhi <hoangphi1232003@gmail.com>
2024-03-28 22:03:55 +0700

    update NhanVien interface

--- Interface/NhanVien_Interface.java
+++ Interface/NhanVien_Interface.java
@@ -6,9 +6,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface NhanVien_Interface{
-    public Boolean checkNV(String email, String sdt) throws SQLException;
-    public NhanVien getNV(String sdt) throws SQLException ;
-    NhanVien selectById(String id);
+    public Boolean checkNV(String email, String sdt);
+    public NhanVien selectById(String id);
     NhanVien findBySDT(String sdt);
     boolean update(NhanVien updateNV);
     boolean insert(NhanVien insertNV);
